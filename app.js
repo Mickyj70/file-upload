da727fa6939482ea087d338e76519146c48f5266
Mickyj77 <emrex1291@gmail.com>
2024-01-02 18:30:12 +0100

    middleware added to add various functionality like file size limit

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 
+//middleware
+const filesPayLoadExist = require("./middleware/filesPayLoadExist");
+const fileExtLimiter = require("./middleware/fileExtLimiter");
+const fileSizeLimiter = require("./middleware/fileSizeLimiter");
+
 const PORT = process.env.PORT || 3500;
 
 const app = express();
@@ -11,12 +16,19 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/upload", fileUpload({ createParentPath: true }), (req, res) => {
-  const files = req.files;
-  console.log(files);
+app.post(
+  "/upload",
+  fileUpload({ createParentPath: true }),
+  filesPayLoadExist,
+  fileExtLimiter([".png", ".jpg", ".jpeg"]),
+  fileSizeLimiter,
+  (req, res) => {
+    const files = req.files;
+    console.log(files);
 
-  return res.json({ status: "logged", message: "logged" });
-});
+    return res.json({ status: "logged", message: "logged" });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
